6bcc5fa92d5e59f7a585d09eefdf737334b5fe6c
faizanmalik <faizan200malik@gmail.com>
2024-03-02 13:06:19 +0500

    otp push

--- lib/services/AuthServices.dart
+++ lib/services/AuthServices.dart
@@ -5,20 +5,65 @@ class AuthService {
 
   // Sign up with email and password
   // Sign up with email and password
-  Future<User?> signUp(String email, String password) async {
+  Future<User?> signUp(String email, String password, String contact) async {
     try {
       UserCredential result = await _auth.createUserWithEmailAndPassword(
           email: email, password: password);
       User? user = result.user;
 
-      if (user != null && !user.emailVerified) {
-        await sendVerificationEmail(user);
+      if (user != null) {
+        // Optionally, link the phone number with the email/password account
+        await _verifyPhoneNumber(contact);
       }
 
       return user;
     } catch (error) {
       print(error.toString());
       return null;
+    }
+  }
+
+  Future<void> _verifyPhoneNumber(String phoneNumber) async {
+    await _auth.verifyPhoneNumber(
+      phoneNumber: phoneNumber,
+      verificationCompleted: (PhoneAuthCredential credential) async {
+        // This callback would get called when verification is done automatically
+      },
+      verificationFailed: (FirebaseAuthException e) {
+        print("Verification failed: ${e.message}");
+      },
+      codeSent: (String verificationId, int? resendToken) async {
+        print("code has been sent check please");
+        // This callback gets called after the OTP is sent.
+        // You can prompt the user to enter the OTP and then create a PhoneAuthCredential
+        // For example: PhoneAuthCredential credential = PhoneAuthProvider.credential(verificationId: verificationId, smsCode: smsCode);
+
+        // Sign in or link with the credential
+        // await _auth.signInWithCredential(credential);
+      },
+      codeAutoRetrievalTimeout: (String verificationId) {
+        // Auto-resolution timed out...
+        print("code time out");
+      },
+    );
+  }
+
+  Future<void> verifyOtp(String verificationId, String smsCode) async {
+    PhoneAuthCredential credential = PhoneAuthProvider.credential(
+        verificationId: verificationId, smsCode: smsCode);
+
+    try {
+      // If you want to link the phone number to the email/password user
+      // final User? currentUser = _auth.currentUser;
+      // await currentUser?.linkWithCredential(credential);
+
+      // Or simply sign in with the phone number
+      final UserCredential userCredential =
+          await _auth.signInWithCredential(credential);
+      final User? user = userCredential.user;
+      print("Phone number verified and user signed in: ${user?.uid}");
+    } catch (e) {
+      print("Failed to verify OTP: $e");
     }
   }
 
