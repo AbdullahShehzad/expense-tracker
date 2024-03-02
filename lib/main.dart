36b6f01b0bd072e06ee0ee5959590adfd63a1838
Arhamss <syed.arhamss1@gmail.com>
2024-03-02 17:38:20 +0500

    aoa sin here

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:wallet_wise/constants/constants.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:wallet_wise/screens/chat_screen.dart';
 import 'package:wallet_wise/screens/main_screen.dart';
+import 'package:wallet_wise/screens/profile_screen.dart';
 import 'package:wallet_wise/screens/profile_settings.dart';
 import 'package:wallet_wise/services/local_notification_service.dart';
 
@@ -26,7 +28,7 @@ class MyApp extends StatelessWidget {
         ),
         useMaterial3: true,
       ),
-      home: const ProfileSettings(),
+      home: const ProfileScreen(),
     );
   }
 }
--- lib/screens/profile_screen.dart
+++ lib/screens/profile_screen.dart
@@ -10,6 +10,182 @@ class ProfileScreen extends StatefulWidget {
 class _ProfileScreenState extends State<ProfileScreen> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    final screenHeight = MediaQuery.of(context).size.height;
+    return Scaffold(
+      backgroundColor: Color(0xFF392800),
+      body: Column(
+        children: [
+          Expanded(
+            flex: 1,
+            child: Container(
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(80.0),
+              ),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  IconButton(
+                    icon: const Icon(
+                      Icons.arrow_back_ios_rounded,
+                    ),
+                    color: Colors.white,
+                    iconSize: 28, // Size of the icon
+                    onPressed: () {
+                      Navigator.pop(context);
+                    },
+                  ),
+                  SizedBox(
+                    width: 20,
+                  ),
+                  const Text(
+                    'Profile',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 20,
+                      fontWeight: FontWeight.w700,
+                    ),
+                  ),
+                  SizedBox(
+                    width: 20,
+                  ),
+                  IconButton(
+                    icon: const Icon(
+                      Icons.notifications_rounded,
+                    ),
+                    color: Colors.white,
+                    iconSize: 28, // Size of the icon
+                    onPressed: () {
+                      print('Icon Button Pressed!');
+                    },
+                  ),
+                ],
+              ),
+            ),
+          ),
+          Expanded(
+            flex: 2,
+            child: Container(
+              width: double.infinity,
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.circular(80.0),
+              ),
+              child: Padding(
+                padding: const EdgeInsets.symmetric(
+                  horizontal: 23,
+                ),
+                child: Column(
+                  children: [
+                    SizedBox(
+                      height: 80,
+                    ),
+                    Text(
+                      "Muhammad Saad",
+                      style: TextStyle(
+                          fontSize: 20,
+                          color: Color(0xFF222222),
+                          fontWeight: FontWeight.bold),
+                    ),
+                    SizedBox(
+                      height: 70,
+                    ),
+                    Row(
+                      children: [
+                        IconButton(
+                          style: IconButton.styleFrom(
+                            backgroundColor: Color(0xFFE9AB17),
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(12),
+                            ),
+                          ),
+                          onPressed: () {},
+                          icon: Icon(
+                            Icons.group_add_rounded,
+                            color: Color(0xFF392800),
+                          ),
+                        ),
+                        SizedBox(
+                          width: 40,
+                        ),
+                        Text(
+                          "Invite Friends",
+                          style: TextStyle(
+                            fontSize: 18,
+                            color: Color(0xFF000000),
+                          ),
+                        )
+                      ],
+                    ),
+                    Divider(
+                      color: Color(0xFF6A5931).withOpacity(.35),
+                    ),
+                    Row(
+                      children: [
+                        IconButton(
+                          style: IconButton.styleFrom(
+                            backgroundColor: Color(0xFFE9AB17),
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(12),
+                            ),
+                          ),
+                          onPressed: () {},
+                          icon: Icon(
+                            Icons.settings,
+                            color: Color(0xFF392800),
+                          ),
+                        ),
+                        SizedBox(
+                          width: 40,
+                        ),
+                        Text(
+                          "Settings",
+                          style: TextStyle(
+                            fontSize: 18,
+                            color: Color(0xFF000000),
+                          ),
+                        )
+                      ],
+                    ),
+                    Divider(
+                      color: Color(0xFF6A5931).withOpacity(.35),
+                    ),
+                    Row(
+                      children: [
+                        IconButton(
+                          style: IconButton.styleFrom(
+                            backgroundColor: Color(0xFFE9AB17),
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(12),
+                            ),
+                          ),
+                          onPressed: () {},
+                          icon: Icon(
+                            Icons.help,
+                            color: Color(0xFF392800),
+                          ),
+                        ),
+                        SizedBox(
+                          width: 40,
+                        ),
+                        Text(
+                          "Customer Support",
+                          style: TextStyle(
+                            fontSize: 18,
+                            color: Color(0xFF000000),
+                          ),
+                        )
+                      ],
+                    ),
+                    Divider(
+                      color: Color(0xFF6A5931).withOpacity(.35),
+                    )
+                  ],
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
